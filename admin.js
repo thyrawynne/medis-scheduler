6c1939fa43a519e1c55e883013b0bf4ae0367503
thyrawynne <thyra.wynne.mandak10@gmail.com>
2025-04-21 17:59:00 +0800

    Server connect

--- admin.js
+++ admin.js
@@ -1,3 +1,24 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  // Menampilkan daftar dokter
+fetch('http://localhost:5000/api/dokter')
+.then(res => res.json())
+.then(data => {
+  data.forEach(dokter => {
+    // tampilkan ke HTML
+    console.log(dokter.nama);
+  });
+});
+
+// Menambahkan dokter baru (form submission)
+function tambahDokter(nama, spesialis, jadwal) {
+fetch('http://localhost:5000/api/dokter', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ nama, spesialis, jadwal })
+})
+  .then(res => res.json())
+  .then(data => alert(data.message));
+}
